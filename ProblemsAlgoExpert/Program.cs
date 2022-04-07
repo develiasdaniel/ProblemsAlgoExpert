3751007ed1dc50d2303e83436657dd47c9c1c4db
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2022-04-07 08:12:41 -0500

    add two sum problem and test

---
+++ ProblemsAlgoExpert/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProblemsAlgoExpert;
+using System.Collections;
+
+Console.WriteLine("Hello, World!, Estas son soluciones a algo expert.");
+var sum1 = TwoNumberSum.TwoNumberSumSolution1(new int[]{ 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
+Console.WriteLine(string.Join(",", sum1));
---
+++ ProblemsAlgoExpert/TwoNumberSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsAlgoExpert
+{
+    public class TwoNumberSum
+    {
+		public static int[] TwoNumberSumSolution1(int[] array, int targetSum)
+		{
+			// Write your code here.
+			for (var x = 0; x < array.Length; x++)
+			{
+				var firstNum = array[x];
+				for (var y = x + 1; y < array.Length; y++)
+				{
+					var secondNum = array[y];
+					if (firstNum + secondNum == targetSum)
+					{
+						return new int[] { firstNum, secondNum };
+					}
+				}
+			}
+			return new int[0];
+		}
+
+		public static int[] TwoNumberSumSolution2(int[] array, int targetSum)
+		{
+			// Write your code here.
+			HashSet<int> nums = new HashSet<int>();
+            foreach (var num in array)
+            {
+				int potentialMatch = targetSum - num;
+				if(nums.Contains(potentialMatch))
+                {
+					return new int[] { potentialMatch, num };
+				}
+                else
+                {
+					nums.Add(num);
+                }
+			}
+			return new int[0];
+		}
+	}
+}
