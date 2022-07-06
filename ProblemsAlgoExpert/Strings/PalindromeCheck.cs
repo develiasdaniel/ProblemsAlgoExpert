12d70f78b9f835460112aa28596ed332afe033ad
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2022-07-06 07:11:44 -0500

    add problem palindrome check
    add four answer and complexity
    add unit test for each answer

---
+++ ProblemsAlgoExpert/Strings/PalindromeCheck.cs
@@ -0,0 +1,75 @@
+﻿namespace ProblemsAlgoExpert.Strings
+{
+    public class PalindromeCheck
+    {
+        // O(n) time | O(1) space
+        public static bool IsPalindrome(string str)
+        {
+            // Write your code here.
+            int indexLeft = 0;
+            int indexRight = str.Length - 1;
+
+            while (indexLeft <= indexRight)
+            {
+                var letterLeft = str[indexLeft];
+                var letterRight = str[indexRight];
+                if (letterLeft == letterRight)
+                {
+                    indexLeft++;
+                    indexRight--;
+                }
+                else
+                {
+                    return false; // not is a palindrome    
+                }
+            }
+            return true;
+        }
+
+        // O(n) time | O(n) space
+        public static bool IsPalindrome2(string str)
+        {
+            // Write your code here.
+            var reverseLetters = new List<char>();
+            for (int idx = str.Length - 1; idx >= 0; idx--)
+            {
+                reverseLetters.Add(str[idx]);
+            }
+            string reverseWord = string.Join("", reverseLetters);
+            return str == reverseWord;
+        }
+
+        // O(n) time | O(n) space
+        public static bool IsPalindrome3(string str)
+        {
+            // Write your code here.
+            char[] wordRightArray = str.ToCharArray();
+            Array.Reverse(wordRightArray);
+            string reverseString = new String(wordRightArray);
+            return str == reverseString;
+        }
+
+        // O(n) time | O(n) space
+        public static bool IsPalindrome4(string str)
+        {
+            // Write your code here.
+            var isEven = str.Length % 2 == 0;
+            int half = str.Length / 2;
+            string wordLeft;
+            if (isEven)
+            {
+                wordLeft = str.Substring(0, half);
+            }
+            else
+            {
+                wordLeft = str.Substring(0, half + 1);
+            }
+
+            char[] wordRightArray = str.Substring(half).ToCharArray();
+            Array.Reverse(wordRightArray);
+            string wordRight = new String(wordRightArray);
+            return wordLeft == wordRight;
+        }
+
+    }
+}
---
+++ TestsAlgoExpert/Strings/PalindromeCheckTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using ProblemsAlgoExpert.Strings;
+
+namespace TestsAlgoExpert.Strings
+{
+    public class PalindromeCheckTest
+    {
+        /// <summary>
+        /// TypeCases.
+        /// </summary>
+        private static readonly (string str, bool result)[] TypeCases =
+        {
+            ( "abcdcba", true),
+            ( "a", true),
+            ( "ab", false),
+            ( "aba", true),
+            ( "abb", false),
+            ( "abba", true),
+            ( "abcdefghhgfedcba", true),
+            ( "abcdefghihgfedcba", true),
+            ( "abcdefghihgfeddcba", false),
+        };
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void IsPalindrome((string str, bool result) test)
+        {
+            bool output = PalindromeCheck.IsPalindrome(test.str);
+            Assert.True(output == test.result);
+        }
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void IsPalindrome2((string str, bool result) test)
+        {
+            bool output = PalindromeCheck.IsPalindrome2(test.str);
+            Assert.True(output == test.result);
+        }
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void IsPalindrome3((string str, bool result) test)
+        {
+            bool output = PalindromeCheck.IsPalindrome3(test.str);
+            Assert.True(output == test.result);
+        }
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void IsPalindrome4((string str, bool result) test)
+        {
+            bool output = PalindromeCheck.IsPalindrome4(test.str);
+            Assert.True(output == test.result);
+        }
+    }
+}
