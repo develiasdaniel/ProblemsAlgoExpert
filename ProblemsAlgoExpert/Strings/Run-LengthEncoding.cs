b7f6b3a59aca07bff14cb4260e504041701146ac
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2022-07-28 23:09:39 -0500

    add run-length encoding problem
    add 2 answeres and complexity
    add unit test for each answer

---
+++ ProblemsAlgoExpert/Strings/Run-LengthEncoding.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace ProblemsAlgoExpert.Strings
+{
+    public static class Run_LengthEncoding
+    {
+        // O(n) time | O(n) space where n is the length of the input string
+        public static string RunLengthEncoding(string str)
+        {
+            // Write your code here.
+            StringBuilder wordEncoded = new StringBuilder();
+            char lastLetter = str[0];
+            int countRepeat = 1;
+
+            if (str.Length == 1)
+            {
+                wordEncoded.Append($"{1}{lastLetter}");
+                return wordEncoded.ToString();
+            }
+            for (int idx = 1; idx < str.Length; idx++)
+            {
+                char actualLetter = str[idx];
+                if (lastLetter == actualLetter)
+                {
+                    if (countRepeat == 9)
+                    {
+                        wordEncoded.Append($"{9}{lastLetter}");
+                        countRepeat = 0;
+                    }
+                }
+                else
+                {
+                    wordEncoded.Append($"{countRepeat}{lastLetter}");
+                    countRepeat = 0;
+                }
+
+                lastLetter = actualLetter;
+                countRepeat++;
+            }
+
+            wordEncoded.Append($"{countRepeat}{lastLetter}");
+            return wordEncoded.ToString();
+        }
+
+        // O(n) time | O(n) space where n is the length of the input string
+        public static string RunLengthEncodingSol2(string str)
+        {
+            StringBuilder wordEncoded = new StringBuilder();
+            int currentRunLength = 1;
+
+            for (int idx = 1; idx < str.Length; idx++)
+            {
+                char previousLetter = str[idx - 1];
+                char actualLetter = str[idx];
+                if ((actualLetter != previousLetter) || (currentRunLength == 9))
+                {
+                    wordEncoded.Append($"{currentRunLength}{previousLetter}");
+                    currentRunLength = 0;
+                }
+                currentRunLength += 1;
+            }
+
+            wordEncoded.Append($"{currentRunLength}{str[str.Length - 1]}");
+            return wordEncoded.ToString();
+        }
+    }
+}
---
+++ TestsAlgoExpert/Strings/Run_LengthEncodingTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using ProblemsAlgoExpert.Strings;
+
+namespace TestsAlgoExpert.Strings
+{
+    public class Run_LengthEncodingTest
+    {
+        /// <summary>
+        /// TypeCases.
+        /// </summary>
+        private static readonly (string input, string expected)[] TypeCases =
+        {
+            ( "AAAAAAAAAAAAABBCCCCDD", "9A4A2B4C2D"),
+            ( "aA", "1a1A"),
+            ( "122333", "112233"),
+            ( "************^^^^^^^$$$$$$%%%%%%%!!!!!!AAAAAAAAAAAAAAAAAAAA", "9*3*7^6$7%6!9A9A2A"),
+            ( "aAaAaaaaaAaaaAAAABbbbBBBB", "1a1A1a1A5a1A3a4A1B3b4B"),
+            ( "                          ", "9 9 8 "),
+            ( "1  222 333    444  555", "112 321 334 342 35"),
+            ( "1A2BB3CCC4DDDD", "111A122B133C144D"),
+            ( "........______=========AAAA   AAABBBB   BBB", "8.6_9=4A3 3A4B3 3B"),
+            ( "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a1a"),
+            ( "        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "8 9a9a9a9a9a4a"),
+            ( " ", "1 "),
+            ( ";;;;;;;;;;;;''''''''''''''''''''1233333332222211112222111s", "9;3;9'9'2'111273524142311s"),
+            ( "AAAAAAAAAAAAABBCCCCDDDDDDDDDDD", "9A4A2B4C9D2D"),
+            ( "[(aaaaaaa,bbbbbbb,ccccc,dddddd)]", "1[1(7a1,7b1,5c1,6d1)1]"),
+        };
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void RunLengthEncoding((string input, string expected) test)
+        {
+            string output = Run_LengthEncoding.RunLengthEncoding(test.input);
+            Assert.True(output == test.expected);
+        }
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void RunLengthEncodingSol2((string input, string expected) test)
+        {
+            string output = Run_LengthEncoding.RunLengthEncodingSol2(test.input);
+            Assert.True(output == test.expected);
+        }
+    }
+}
