1f834c11f6046c66fb799d710f188a3ce4272cbc
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2022-08-10 21:28:36 -0500

    add generate document problem
    add 3 answeres and complexity
    add unit test for each answer

---
+++ ProblemsAlgoExpert/Strings/GenerateDocument.cs
@@ -0,0 +1,90 @@
+﻿namespace ProblemsAlgoExpert.Strings
+{
+    public static class GenerateDocument
+    {
+        // O(n * (m + m)) time | O(1) space
+        // where n is document size and m characters size
+        public static bool GenerateDocumentSolution(string characters, string document)
+        {
+            // Write your code here.
+            for (int idx = 0; idx < document.Length; idx++)
+            {
+                char letterDocument = document[idx];
+                int indexCharacter = characters.IndexOf(letterDocument);
+                if (indexCharacter == -1)
+                {
+                    return false;
+                }
+
+                characters = characters.Remove(indexCharacter, 1);
+            }
+            return true;
+        }
+
+        // O(n + m) time | O (c) space 
+        // where n is characters size and n document size 
+        // and c is the size of unique characters in characters string
+        public static bool GenerateDocumentSolution2(string characters, string document)
+        {
+            // Write your code here.
+            var characterCounts = new Dictionary<char, int>();
+            foreach (var letter in characters)
+            {
+                characterCounts[letter] =
+                    characterCounts.GetValueOrDefault(letter, 0) + 1;
+            }
+
+            foreach (var letter in document)
+            {
+                if (!characterCounts.ContainsKey(letter) || characterCounts[letter] == 0)
+                {
+                    return false;
+                }
+
+                characterCounts[letter] = characterCounts[letter] - 1;
+            }
+            return true;
+        }
+
+
+        // O(n + m) time | O (c) space 
+        // where n is characters size and n document size 
+        // and c is the size of unique characters in characters string
+        public static bool GenerateDocumentSolution3(string characters, string document)
+        {
+            // Write your code here.
+            var alreadyCounted = new HashSet<char>();
+
+            foreach (var letter in document)
+            {
+                if (alreadyCounted.Contains(letter))
+                {
+                    continue;
+                }
+
+                int documentFrequency = countcharFrequency(letter, document);
+                int characterfrequency = countcharFrequency(letter, characters);
+                if(documentFrequency > characterfrequency)
+                {
+                    return false;
+                }
+                alreadyCounted.Add(letter);
+            }
+            return true;
+        }
+
+        private static int countcharFrequency(char character, string target)
+        {
+            int frequency = 0;
+            foreach (var letter in target)
+            {
+                if(letter == character)
+                {
+                    frequency++;
+                }
+            }
+
+            return frequency;
+        }
+    }
+}
---
+++ TestsAlgoExpert/Strings/GenerateDocumentTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using ProblemsAlgoExpert.Strings;
+
+namespace TestsAlgoExpert.Strings
+{
+    public class GenerateDocumentTest
+    {
+        /// <summary>
+        /// TypeCases.
+        /// </summary>
+        private static readonly (string character, string document, bool expected)[] TypeCases =
+        {
+            ( "Bste!hetsi ogEAxpelrt x ", "AlgoExpert is the Best!", true),
+            ( "A", "a", false),
+            ( "a", "a", true),
+            ( "a hsgalhsa sanbjksbdkjba kjx", "", true),
+            ( " ", "hello", false),
+            ( "     ", "     ", true),
+            ( "aheaollabbhb", "hello", true),
+            ( "aheaolabbhb", "hello", false),
+            ( "estssa", "testing", false),
+            ( "Bste!hetsi ogEAxpert", "AlgoExpert is the Best!", false),
+            ( "helloworld ", "hello wOrld", false),
+            ( "helloworldO", "hello wOrld", false),
+            ( "helloworldO ", "hello wOrld", true),
+            ( "&*&you^a%^&8766 _=-09     docanCMakemthisdocument", "Can you make this document &", true),
+            ( "abcabcabcacbcdaabc", "bacaccadac", true),
+        };
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void GenerateDocumentSolution((string character, string document, bool expected) test)
+        {
+            bool output = GenerateDocument.GenerateDocumentSolution(test.character, test.document);
+            Assert.True(output == test.expected);
+        }
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void GenerateDocumentSolution2((string character, string document, bool expected) test)
+        {
+            bool output = GenerateDocument.GenerateDocumentSolution2(test.character, test.document);
+            Assert.True(output == test.expected);
+        }
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void GenerateDocumentSolution3((string character, string document, bool expected) test)
+        {
+            bool output = GenerateDocument.GenerateDocumentSolution3(test.character, test.document);
+            Assert.True(output == test.expected);
+        }
+    }
+}
