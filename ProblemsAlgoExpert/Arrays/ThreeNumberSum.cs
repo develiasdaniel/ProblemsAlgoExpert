f4573ba68c3fad7f42c11e50665df41f911ee7d3
Elías D Sánchez <69357302+develiasdaniel@users.noreply.github.com>
2022-09-12 08:49:00 -0500

    three number sum problem
    add two answer and complexity
    add unit test for each answer

---
+++ ProblemsAlgoExpert/Arrays/ThreeNumberSum.cs
@@ -0,0 +1,68 @@
+﻿namespace ProblemsAlgoExpert.Arrays
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class ThreeNumberSum
+    {
+        // O(n ^ 3) time | O(n) space where n is array size 
+        public static List<int[]> ThreeNumberSumSol1(int[] array, int targetSum)
+        {
+            // Write your code here.
+            var results = new List<int[]>();
+            for (int idx = 0; idx < array.Length - 2; idx++)
+            {
+                int numberOne = array[idx];
+                for (int idxNumberTwo = idx + 1; idxNumberTwo < array.Length - 1; idxNumberTwo++)
+                {
+                    int numberTwo = array[idxNumberTwo];
+                    for (int idxNumTree = idxNumberTwo + 1; idxNumTree < array.Length; idxNumTree++)
+                    {
+                        int numberTree = array[idxNumTree];
+                        int sum = numberOne + numberTwo + numberTree;
+                        if (sum == targetSum)
+                        {
+                            var sumArray = new int[] { numberOne, numberTwo, numberTree };
+                            Array.Sort(sumArray);
+                            results.Add(sumArray);
+                        }
+                    }
+                }
+            }
+            return results.OrderBy(x => x[0]).ThenBy(x => x[1]).ThenBy(x => x[2]).ToList();
+        }
+
+        // O(n ^ 2) time | O(n) space
+        public static List<int[]> ThreeNumberSumSol2(int[] array, int targetSum)
+        {
+            // Write your code here.
+            List<int[]> triplets = new List<int[]>();
+
+            Array.Sort(array);
+            for (int idx = 0; idx < array.Length - 2; idx++)
+            {
+                int leftIdx = idx + 1;
+                int rightIdx = array.Length - 1;
+                while (leftIdx < rightIdx)
+                {
+                    int currentSum = array[idx] + array[leftIdx] + array[rightIdx];
+                    if (currentSum == targetSum)
+                    {
+                        triplets.Add(new int[] { array[idx], array[leftIdx], array[rightIdx] });
+                        leftIdx++;
+                        rightIdx--;
+                    }
+                    else if (currentSum < targetSum)
+                    {
+                        leftIdx++;
+                    }
+                    else
+                    {
+                        rightIdx--;
+                    }
+                }
+            }
+            return triplets;
+        }
+    }
+}
---
+++ TestsAlgoExpert/Arrays/ThreeNumberSumTest.cs
@@ -0,0 +1,132 @@
+﻿namespace TestsAlgoExpert.Arrays
+{
+    using NUnit.Framework;
+    using ProblemsAlgoExpert.Arrays;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ThreeNumberSumTest
+    {
+        /// <summary>
+        /// TypeCases.
+        /// </summary>
+        private static readonly (List<int[]> expected, int[] array, int targetSum)[] TypeCases =
+        {
+            (new List<int[]>
+            {
+                new int[]{-8, 2, 6},
+                new int[]{-8, 3, 5},
+                new int[]{ -6, 1, 5 }
+            },
+            new int[]{ 12, 3, 1, 2, -6, 5, -8, 6 },
+            0),
+            (new List<int[]>
+            {
+                new int[]{1, 2, 3},
+            },
+            new int[]{ 1, 2, 3 },
+            6),
+            (new List<int[]>
+            {
+            },
+            new int[]{ 1, 2, 3 },
+            7),
+            (new List<int[]>
+            {
+                new int[]{ -2, 10, 49 },
+            },
+            new int[]{ 8, 10, -2, 49, 14 },
+            57),
+            (new List<int[]>
+            {
+                new int[]{ -8, 3, 5 },
+                new int[]{ -6, 1, 5 },
+                new int[]{ -1, 0, 1 },
+            },
+            new int[]{ 12, 3, 1, 2, -6, 5, 0, -8, -1 },
+            0),
+            (new List<int[]>
+            {
+                new int[]{ -8, 2, 6 },
+                new int[]{ -8, 3, 5 },
+                new int[]{ -6, 0, 6 },
+                new int[]{ -6, 1, 5 },
+                new int[]{ -1, 0, 1 },
+            },
+            new int[]{ 12, 3, 1, 2, -6, 5, 0, -8, -1, 6 },
+            0),
+            (new List<int[]>
+            {
+                new int[]{ -8, 2, 6 },
+                new int[]{ -8, 3, 5},
+                new int[]{ -6, 0, 6 },
+                new int[]{ -6, 1, 5 },
+                new int[]{ -5, -1, 6 },
+                new int[]{ -5, 0, 5 },
+                new int[]{ -5, 2, 3 },
+                new int[]{ -1, 0, 1 },
+            },
+            new int[]{ 12, 3, 1, 2, -6, 5, 0, -8, -1, 6, -5 },
+            0),
+            (new List<int[]>
+            {
+                new int[]{ 1, 2, 15 },
+                new int[]{ 1, 8, 9 },
+                new int[]{ 2, 7, 9 },
+                new int[]{ 3, 6, 9 },
+                new int[]{ 3, 7, 8 },
+                new int[]{ 4, 5, 9 },
+                new int[]{ 4, 6, 8 },
+                new int[]{ 5, 6, 7 },
+            },
+            new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 15 },
+            18),
+            (new List<int[]>
+            {
+                new int[]{ 8, 9, 15 },
+            },
+            new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 15 },
+            32),
+            (new List<int[]>
+            {
+            },
+            new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 15 },
+            33),
+            (new List<int[]>
+            {
+            },
+            new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 15 },
+            5),
+        };
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void ThreeNumberSumSol1((List<int[]> expected, int[] array, int targetSum) test)
+        {
+            List<int[]> output = ThreeNumberSum.ThreeNumberSumSol1(test.array, test.targetSum);
+            Assert.True(this.compare(output, test.expected));
+        }
+
+        [Test]
+        [TestCaseSource("TypeCases")]
+        public void ThreeNumberSumSol2((List<int[]> expected, int[] array, int targetSum) test)
+        {
+            List<int[]> output = ThreeNumberSum.ThreeNumberSumSol2(test.array, test.targetSum);
+            Assert.True(this.compare(output, test.expected));
+        }
+
+        private bool compare(List<int[]> triplets1, List<int[]> triplets2)
+        {
+            if (triplets1.Count != triplets2.Count) return false;
+            for (int i = 0; i < triplets1.Count; i++)
+            {
+                if (!Enumerable.SequenceEqual(triplets1[i], triplets2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
